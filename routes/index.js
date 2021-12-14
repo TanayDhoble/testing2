6aa9a6c252ea702eda6b91414839061ed85af141
TanayCDhoble <tanudhoble13@gmail.com>
2021-12-14 08:36:19 +0530

    routes

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var router = express.Router();
+// var router = express.Router();
 const ObjectID = require('mongodb').ObjectID;
 
 router.get('/appointments', (req, res, next) => {
@@ -10,26 +10,37 @@ router.get('/appointments', (req, res, next) => {
 });
 
 router.post('/appointments', (req, res, next) => {
-  const { appointmentDate, name, email } = req.body;
+  const {
+    appointmentDate,
+    name,
+    email
+  } = req.body;
   if (!appointmentDate || !name || !email) {
     return res.status(400).json({
       message: 'Appointment Date, Name and email are required',
     });
   }
 
-  const payload = { appointmentDate, name, email };
+  const payload = {
+    appointmentDate,
+    name,
+    email
+  };
   req.collection.insertOne(payload)
     .then(result => res.json(result.ops[0]))
     .catch(error => res.send(error));
 });
 
 router.delete('/appointments/:id', (req, res, next) => {
-  const { id } = req.params;
+  const {
+    id
+  } = req.params;
   const _id = ObjectID(id);
-  req.collection.deleteOne({ _id })
+  req.collection.deleteOne({
+      _id
+    })
     .then(result => res.json(result))
     .catch(error => res.send(error));
 });
 
 module.exports = router;
-
